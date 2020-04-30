12427765425aac05612f797e39300bbd8e132a72
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-30 17:03:13 -0500

    first commit

---
+++ box_color/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>testing</p>
+        
+    </div>
+  );
+}
+
+export default App;
