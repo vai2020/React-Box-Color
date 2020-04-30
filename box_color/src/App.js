e136cb25e676b40b0e4c71233b159cf41301c25b
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-30 17:37:53 -0500

    create this.state and onClick event

--- box_color/src/App.js
+++ box_color/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+    constructor(props) {             //constructor set up for State
+      super(props)
+      this.state={         }         //initialize State
+    }
+          
   render() {
     return (
     <div className="App">
-     <div className="color">   </div>
-        
+     <div className="color" onClick={this.toggle}   >   </div>  //create a box and added toggle function//
+
+
     </div>
   );
 }
