32634a83f3d2d865d1709f727f473443b23ae05f
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-30 17:11:26 -0500

    cleaned up App.js and App.css

--- box_color/src/App.js
+++ box_color/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
-      <p>testing</p>
+      <p>       </p>
         
     </div>
   );
 }
+
+  }
+
+     
+  
 
 export default App;
