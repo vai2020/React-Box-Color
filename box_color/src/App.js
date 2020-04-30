2c455bec2199af588575e88110efd5204b76feaf
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-30 18:01:57 -0500

    added toggle method to test color change

--- box_color/src/App.js
+++ box_color/src/App.js
@@ -4,14 +4,20 @@ import './App.css';
 class App extends React.Component {
     constructor(props) {             //constructor set up for State
       super(props)
-      this.state={color:"blue"       }         //this.state={}initialize State
+      this.state={color:"blue"}         //this.state={}initialize State
+    } 
+    toggle=() => {
+      this.setState ({color:"pink"})
     }
           
   render() {
     return (
     <div className="App">
-     <div className="toggleBox" onClick={this.toggle}   >   </div>  //create a box and added toggle function//
+     <div className="toggleBox" 
+     onClick={this.toggle}  style={{backgroundColor:this.state.color}} >   </div>  
+    
 
+     {/* /* onClick={this.toggle}create a box and added toggle method */ }
 
     </div>
   );
