5207f5cf50390c21f2cb4d5d6051d58755f850b5
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-05-02 08:52:39 -0500

    add dancing script google fonts in html and css

--- box_color/src/App.js
+++ box_color/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
 class App extends React.Component {
     constructor(props) {             //constructor set up for State
       super(props)
       this.state={color:"blue", counter:0}         //this.state={}initialize State
-      
+     
+      /*the following block of codes bind the function to change the box color 
+      and update the color */
     } 
     toggle=() => {
       this.setState ({color:"pink", counter:this.state.counter+1})
@@ -16,10 +19,10 @@ class App extends React.Component {
     }
     render() {
       console.log(this.state.counter)   
-    
+    //the following block of codes render the toggle box when onClick event is invoked
     return (
     <div className="App">
-      <h1 className="c">{this.state.counter}   </h1>
+      <h1 id="c"><p>counter: {this.state.counter}</p>  </h1>
      <div className="toggleBox" 
      onClick={this.toggle}  style={{backgroundColor:this.state.color}} >   </div>  
     
